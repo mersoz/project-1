e1a3afec4504a8c168392d899a55809c98896361
Muge Ersoz <muge@ersoz.org>
2017-02-14 16:33:24 +0000

    Shapes can move

--- js/app.js
+++ js/app.js
@@ -2,79 +2,86 @@ console.log('JS loaded');
 
 $(() => {
   // Create and implement a grid
-  const $gameboard = $('.gameboard');
   const width = 12;
   const height = 20;
   const numberOfGrids = width*height;
-  let index = Math.floor((width-1)/2);
 
+  const $gameboard = $('.gameboard');
+  for (var i = 0; i < numberOfGrids; i++) {
+    var $gridblock = $('<div>', {'class': 'gridblock'});
+    $gameboard.append($gridblock);
+  }
+  const $allGrids = $('div.gridblock');
+
+  // Initialize stuff
+  let initIndices = [];
+  let index = Math.floor((width-1)/2);    //start dropping from middle of board
+  const dropping = setInterval(dropPiece, 100);
+
+  // Create an array for each rown with indexes
+    // Push each array into another for all arrays/rows
   const arrayOfRows = [];   //big array with 20 other arrays
   for ( let initNum = 0; initNum < numberOfGrids; initNum+=width ) {
-    // console.log(`Begin array with: ${initNum}`);
     const oneRow = []; //small array with index of each square on row
     for ( let indexOfEach = initNum; indexOfEach < initNum+width; indexOfEach++) {
       oneRow.push(indexOfEach);
     }
     arrayOfRows.push(oneRow);
   }
-  // console.log(arrayOfRows);
 
-  for (var i = 0; i < numberOfGrids; i++) {
-    var $gridblock = $('<div>', {'class': 'gridblock'});
-    $gameboard.append($gridblock);
-  }
 
-  const $allGrids = $('div.gridblock');
 
-  //start dropping from middle of board
-  //drop piece one row down every second
-  const dropping = setInterval(dropPiece, 100);
   function dropPiece(){
-    var indices = spawnPiece();
+    initIndices = spawnPiece();
+
     // console.log(`Moving piece is on index: ${index}`);
     // clear previous location of moving piece and
     $allGrids.removeClass('movingPiece');
-    for (var p = 0; p < indices.length; p++) {
-      $allGrids.eq(indices[p]).addClass('movingPiece');
+    for (var p = 0; p < initIndices.length; p++) {
+      $allGrids.eq(initIndices[p]).addClass('movingPiece');
+    }
+    console.log(initIndices);
+
+    function nextRowFull(){
+      const occ = [];
+      for (var g = 0; g < initIndices.length; g++) {
+        occ.push($allGrids.eq(initIndices[g]+width).hasClass('occupied'));
+      }
+      if (occ.some(isSquareFull)) {
+        return true;
+      }
     }
 
-    //if next row is not occupied, move one down
-    if (index < numberOfGrids-width && !$allGrids.eq(index+width).hasClass('occupied')) {
+    function canGoDown() {
+    //lowest value when divisible by width
+      return initIndices.every((index) => {
+        return index < numberOfGrids - width;
+      });
+    }
+
+    // if (index < numberOfGrids-width && !nextRowFull()) {
+    if (canGoDown() && !nextRowFull()) {
       index += width;         // console.log('move one down');
     } else {                  //if on top row and next row is not occupied
-      for (var g = 0; g < indices.length; g++) {
-        $allGrids.eq(indices[g]).addClass('occupied');
+      for (var g = 0; g < initIndices.length; g++) {
+        $allGrids.eq(initIndices[g]).addClass('occupied');
+        console.log('occupy');
       }
 
       // $allGrids.eq(index).addClass('occupied');
       checkRemoveFullRowsDropAll();
 
-      if (index<width) {      // if on top row
-        //end game when top row is occupied
+      if (index<width) {      // end game if on top row
         //--> should clearInterval before new piece ?
         $allGrids.removeClass('movingPiece');
-        // console.log('end game');
-
         clearInterval(dropping);
       } else {                // if next row is occupied
-        // console.log('next div full OR reached last row');
-        // console.log(index);
         index = Math.floor((width-1)/2);
       }
     }
 
-    function spawnPiece() {
-      const shapesAvailable = {
-        I: [index, index+width, index+(2*width), index+(3*width)],
-        O: [index, index+1, index+width+1, index+width],
-        T: [index, index+width+1, index+width, index+width-1],
-        L: [index, index+width, index+(2*width), index+(2*width)+1],
-        J: [index, index+width, index+(2*width), index+(2*width)-1],
-        S: [index, index+1, index+width, index+width-1],
-        Z: [index, index-1, index+width, index+width+1]
-      };
-      //return random property
-      return shapesAvailable['S'];
+    function isSquareFull(element) {
+      return element === true;
     }
 
     function checkRemoveFullRowsDropAll() {
@@ -82,12 +89,8 @@ $(() => {
         var isRowFull = thisArray.map( (indexNumber) => {
           return $allGrids.eq(indexNumber).hasClass('occupied');
         }); //returns array of true and false
-
-        function isSquareFull(element) {
-          return element === true;
-        }
-
         if (isRowFull.every(isSquareFull)) {
+          console.log(isRowFull);
           let l = $allGrids.length;
           while (l > width) {
             var classToChangeTo = $allGrids.eq(l-width).attr('class');
@@ -97,22 +100,72 @@ $(() => {
         }
       });
     }
+
   }
-  //add event listener to window (this) to handle user keystrokes
-  //or should bind?
-  $(this).keydown((e) => {
-    // console.log(e.which);
-    // left 37
-    if (e.which === 37 && index % width !== 0 ) {           // LEFT
-      //while index is not the left-most div
+
+  function spawnPiece() {
+    const shapesAvailable = {
+      I: [index, index+width, index+(2*width), index+(3*width)],
+      O: [index, index+width, index+1, index+width+1],
+      T: [index, index+width, index+width+1, index+width-1],
+      L: [index, index+width, index+(2*width), index+(2*width)+1],
+      J: [index, index+width, index+(2*width), index+(2*width)-1],
+      S: [index, index+width, index+1, index+width-1],
+      Z: [index, index+width, index-1, index+width+1]
+    };
+    //return random property
+    // const arrOfShapeOptions = Object.keys(shapesAvailable);
+    // const randomNum = Math.floor( Math.random() * arrOfShapeOptions.length);
+    // const randomShape = arrOfShapeOptions[randomNum];
+    // return shapesAvailable[randomShape];
+    return shapesAvailable['T'];
+  }
+
+  function canGoLeft() {
+    //check for occupied one left (index-1)
+    //lowest value when divisible by width
+    return initIndices.every((index) => {
+      return index % width !== 0;
+    });
+  }
+
+  function canGoRight() {
+  //lowest value when divisible by width
+    return initIndices.every((index) => {
+      return index % width !== width - 1;
+    });
+  }
+
+  // Listeners for arrow keys
+  $(this).keydown((e) => {          // should bind?
+    // if (e.which === 37 && checkLeft ) {   //while index is not the left-most div
+    if ( e.which === 37 && canGoLeft() ) {   //while index is not the left-most div
       index-=1;
-    } else if (e.which === 39 && index % width !== width - 1 ) {    // RIGHT
-      //while index is not the right-most div
+    // } else if (e.which === 39  &&  index % width !== width - 1 ) {  //while index is not the right-most div
+    } else if ( e.which === 39 && canGoRight() ) {  //while index is not the right-most div
       index+=1;
-    } else if (e.which === 40) {    // DOWN
+    } else if ( e.which === 40 ) {    //move one rown down
       index+=width;
     }
-  // up 38 - turn shape
-
+    // up 38 - turn shape
   });
+
 });
+
+
+// for (let i = 0; i < initIndices.length; i++) {
+//   const closeToLeft = initIndices.map( (leftIndex) => {
+//     return leftIndex % width;
+//   });
+//   //if touching left
+//   const leftMostValue = Math.min.apply(Math, closeToLeft);
+//   if (leftMostValue !== 0) {
+//     console.log('hi');
+//     return true;
+//   } else {
+//     console.log('touching the left most div');
+//     return false;
+//   }
+//   //console.log(closeToLeft);
+//   //return index with left-most value
+// }
---
+++ test.js
@@ -0,0 +1,24 @@
+// function checkLeft() {
+// //lowest value when divisible by width
+//   for (let i = 0; i < indices.length; i++) {
+//     const closeToLeft = indices.map( (leftIndex) => {
+//       return leftIndex % width;
+//     });
+//     //if touching left
+//     const leftMostValue = Math.min.apply(Math, closeToLeft);
+//     if (leftMostValue !== 0) {
+//       return true;
+//     }
+//
+//     //console.log(closeToLeft);
+//     console.log();
+//     //return index with left-most value
+//   }
+// }
+
+// function checkRight() {
+//
+// }
+
+//checkLeft();
+// checkRight();
