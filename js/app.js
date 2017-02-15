0b40da74111ea6ea2c50b4929cc7159b7c9d9073
Muge Ersoz <muge@ersoz.org>
2017-02-15 21:47:20 +0000

    Pre-simplifying of dropPiece function => either incorrect index on turn (feature) or shapes wont drop (bug)

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ $(() => {
 
   // Initialize stuff
   let rotationIndex = 0;
-  let initIndices = [];
+  let currentRotation = [];
   let index = Math.floor((width-1)/2);    //start dropping from middle of board
   const dropping = setInterval(dropPiece, 200);
 
@@ -62,56 +62,61 @@ $(() => {
          [index, index+1, index+2, index+width+1],
          [index+1, index+width, index+width+1, index+(2*width)+1]]
     };
-
     return shapesAvailable[name];
   }
 
   function dropPiece(){
-    console.log('dropping');
-    initIndices = rotationIndices[rotationIndex];
+    // console.log('dropping');
+    currentRotation = rotationIndices[rotationIndex];
     // clear previous location of moving piece and
     $allGrids.removeClass('movingPiece');
-    for (var p = 0; p < initIndices.length; p++) {
-      $allGrids.eq(initIndices[p]).addClass('movingPiece');
+    for (var p = 0; p < currentRotation.length; p++) {
+      $allGrids.eq(currentRotation[p]).addClass('movingPiece');
     }
 
+
     function canGoDown() { //when not on bottom row
-      return initIndices.every((index) => {
+      return currentRotation.every((index) => {
         return index < numberOfGrids - width;
       });
     }
 
     function nextRowFull(){
       const occ = [];
-      for (var g = 0; g < initIndices.length; g++) {
-        occ.push($allGrids.eq(initIndices[g]+width).hasClass('occupied'));
+      for (var g = 0; g < currentRotation.length; g++) {
+        occ.push($allGrids.eq(currentRotation[g]+width).hasClass('occupied'));
       }
       if (occ.some(isSquareFull)) {
         return true;
       }
     }
 
+    //GO DOWN ONE ROW
     if (canGoDown() && !nextRowFull()) {
-      for (var g = 0; g < initIndices.length; g++) {
-        initIndices[g]+=width;  // console.log('move one down');
+      index+=width;
+      for (var g = 0; g < currentRotation.length; g++) {
+        currentRotation[g]+=width;  // console.log('move one down');
+        // index+=width;  // console.log('move one down');
       }
+      // console.log(rotationIndices);
+
     } else {                  //if on top row and next row is not occupied
-      for (var y = 0; y < initIndices.length; y++) {
-        $allGrids.eq(initIndices[y]).addClass('occupied');
+      for (var y = 0; y < currentRotation.length; y++) {
+        $allGrids.eq(currentRotation[y]).addClass('occupied');
         randomShapeName = shapeNames[Math.floor(Math.random() * shapeNames.length)];
-        rotationIndices = spawnPiece(randomShapeName, index);
+        rotationIndices = spawnPiece(randomShapeName, index); //NEW SHAPE
         rotationIndex = 0;
       }
 
       checkRemoveFullRowsDropAll();
-      console.log(index);
 
-      if (initIndices.every((index) => {     //lowest value
-        return index < width;
-      })) {      // end game if on top row
+      if (Math.min.apply(Math, currentRotation) < width) {      // end game if on top row
         //--> should clearInterval before new piece ?
         $allGrids.removeClass('movingPiece');
         clearInterval(dropping);
+        setTimeout( () => {
+          alert('Game over');
+        }, 500);
       } else {                // if next row is occupied
         index = Math.floor((width-1)/2);
         randomShapeName = shapeNames[Math.floor(Math.random() * shapeNames.length)];
@@ -147,23 +152,23 @@ $(() => {
   }
 
   function canGoLeft() {
-    for (var l = 0; l < initIndices.length; l++) {     //check for occupied one left (index-1)
-      if ($allGrids.eq(initIndices[l]-1).hasClass('occupied')) {
+    for (var l = 0; l < currentRotation.length; l++) {     //check for occupied one left (index-1)
+      if ($allGrids.eq(currentRotation[l]-1).hasClass('occupied')) {
         return false;
       }
     }
-    return initIndices.every((index) => {     //lowest value
+    return currentRotation.every((index) => {     //lowest value
       return index % width !== 0;
     });
   }
 
   function canGoRight() {
-    for (var l = 0; l < initIndices.length; l++) {   //check for occupied one left (index-1)
-      if ($allGrids.eq(initIndices[l]+1).hasClass('occupied')) {
+    for (var l = 0; l < currentRotation.length; l++) {   //check for occupied one left (index-1)
+      if ($allGrids.eq(currentRotation[l]+1).hasClass('occupied')) {
         return false;
       }
     }
-    return initIndices.every((index) => {  //hightest value
+    return currentRotation.every((index) => {  //hightest value
       return index % width !== width - 1;
     });
   }
@@ -172,19 +177,30 @@ $(() => {
   $(this).keydown((e) => {          // should bind?
     // if (e.which === 37 && checkLeft ) {   //while index is not the left-most div
     if ( e.which === 37 && canGoLeft() ) {   //while index is not the left-most div
-      for (var g = 0; g < initIndices.length; g++) {
-        initIndices[g]-=1;
+      for (var b = 0; b < currentRotation.length; b++) {
+        currentRotation[b]-=1;
+        // rotationIndices.forEach( (m, i, a) => {
+        //   i.forEach( (m, i, a) => {
+        //     a[i]-=1;
+        //   });
+        //   console.log(index);
+        // });
+
+
+        // rotationIndices[b][b]-=1;
+        // rotationIndices[g][b] -= 1;
       }
+
     // } else if (e.which === 39  &&  index % width !== width - 1 ) {  //while index is not the right-most div
     } else if ( e.which === 39 && canGoRight() ) {  //while index is not the right-most div
-      for (var u = 0; u < initIndices.length; u++) {
-        initIndices[u]+=1;
+      for (var u = 0; u < currentRotation.length; u++) {
+        currentRotation[u]+=1;
       }
     } else if ( e.which === 40 ) {    //move one rown down
-      for (var p = 0; p < initIndices.length; p++) {
-        initIndices[p]+=2*width;
+      for (var p = 0; p < currentRotation.length; p++) {
+        currentRotation[p]+=2*width;
       }
-    } else if ( e.which === 87 ) {
+    } else if ( e.which === 87 ) { // w
       rotationIndex++;
       rotationIndex = rotationIndex % rotationIndices.length;
     } else if ( e.which === 81 ) {
